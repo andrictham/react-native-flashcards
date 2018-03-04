7d55e2012445ef10e687e853775c81b261c060ec
Andric Tham <andric@nuisance.co>
2018-03-04 18:13:20 +0800

    🎉 Initial commit: Create React Native App + ESLint setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	parser: 'babel-eslint',
+	plugins: ['eslint-plugin-react', 'react-native', 'babel'],
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+		'react-native/react-native': true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			jsx: true,
+		},
+		sourceType: 'module',
+		allowImportExportEverywhere: false,
+		codeFrame: false,
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/jsx-uses-react': 1,
+		'react/jsx-uses-vars': 1,
+		'react-native/no-unused-styles': 2,
+		'react-native/split-platform-components': 2,
+		'react-native/no-inline-styles': 'off',
+		'react/prop-types': 'off',
+		'react/display-name': 'off',
+	},
+}
