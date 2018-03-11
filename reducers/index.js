b7c63c62c5ddbbbf7a15e2803be6d0e2d1baf287
Andric Tham <andric@nuisance.co>
2018-03-11 22:22:40 +0800

    :construction: Chore: Change UUID package to `uuid`

--- reducers/index.js
+++ reducers/index.js
@@ -1,13 +1,13 @@
 import { ADD_DECK, ADD_FLASHCARD } from '../actions'
 import { combineReducers } from 'redux'
 import { initialDecks } from '../utils/helpers'
-import uuid from 'react-native-uuid'
+import uuidv4 from 'uuid/v4'
 
 const decks = (state = initialDecks, action) => {
 	switch (action.type) {
 	case ADD_DECK:
 		const { title } = action
-		const uniqueID = uuid.v4()
+		const uniqueID = uuidv4()
 		return {
 			[uniqueID]: {
 				id: uniqueID,
