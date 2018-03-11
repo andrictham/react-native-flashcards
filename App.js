dd11532e43abe8b6e12ff8ac768b325993c073d2
Andric Tham <andric@nuisance.co>
2018-03-11 23:32:55 +0800

    :sparkles: Feature: Persist state to device storage

--- App.js
+++ App.js
@@ -3,31 +3,37 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import styled from 'styled-components/native'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import CustomStatusBar from './components/CustomStatusBar'
 import MainNavigator from './components/Navigation'
 import COLORS from './styles/colors'
+import configureStore from './utils/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
 
 // TODO: Store everything in Redux
 // TODO: Persist Redux store to AsyncStorage
 // TODO: Add daily push notification reminder
 
-const store = createStore(
-	rootReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-)
+const { store, persistor } = configureStore()
+
+// const store = createStore(
+// 	rootReducer,
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// )
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<AppContainer>
-					<CustomStatusBar
-						backgroundColor={COLORS.accent}
-						barStyle="light-content"
-					/>
-					<MainNavigator style={{ flex: 1 }} />
-				</AppContainer>
+				<PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+					<AppContainer>
+						<CustomStatusBar
+							backgroundColor={COLORS.accent}
+							barStyle="light-content"
+						/>
+						<MainNavigator style={{ flex: 1 }} />
+					</AppContainer>
+				</PersistGate>
 			</Provider>
 		)
 	}
---
+++ utils/configureStore.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import { AsyncStorage } from 'react-native'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+import rootReducer from '../reducers'
+
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+	stateReconciler: autoMergeLevel2,
+	whitelist: ['decks'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+	let store = createStore(
+		persistedReducer,
+		undefined,
+		window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__(),
+	)
+	let persistor = persistStore(store)
+	return { store, persistor }
+}
